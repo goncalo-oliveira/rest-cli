393bbe705b1c1c8340be0462dee91df449e21711
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2022-03-16 15:09:49 +0100

    publishing as single file

--- run-publish.sh
+++ run-publish.sh
@@ -2,15 +2,15 @@
 
 # osx x64
 echo "Publishing OSX x64..."
-dotnet publish -c release --no-self-contained /p:PublishSingleFile=true -o published -r osx-x64 src/rest.csproj
+dotnet publish -c release --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link -o published -r osx-x64 src/rest.csproj
 zip -r -j published/rest-osx-x64.zip published/rest
 
 # windows x64
 echo "Publishing Windows x64..."
-dotnet publish -c release --no-self-contained /p:PublishSingleFile=true -o published -r win10-x64 src/rest.csproj
+dotnet publish -c release --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link -o published -r win10-x64 src/rest.csproj
 zip -r -j published/rest-win10-x64.zip published/rest.exe
 
 # linux x64
 echo "Publishing Linux x64..."
-dotnet publish -c release --no-self-contained /p:PublishSingleFile=true -o published -r linux-x64 src/rest.csproj
+dotnet publish -c release --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link -o published -r linux-x64 src/rest.csproj
 zip -r -j published/rest-linux-x64.zip published/rest
